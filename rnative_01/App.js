ddc5586e6e6662846709434e6584b3ae13bda6a5
Nas <nassersanou23@gmail.com>
2019-07-16 23:47:18 -0400

    updates

--- rnative_01/App.js
+++ rnative_01/App.js
@@ -1,61 +1,114 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import { Navigation } from 'react-native-navigation';
+import ConfigureStore from './src/components/Store/config';
+import { Provider } from 'react-redux';
 
-import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
-import Nav from './src/Nav';
-import Generator from './src/Generator';
-// const instructions = Platform.select({
-//   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-//   android:
-//     'Double tap R on your keyboard to reload,\n' +
-//     'Shake or press menu button for dev menu'
-// });
+import Login from './src/components/views/Login';
+import Home from './src/components/views/Home';
+import AddPost from './src/components/views/Admin/AddPost';
+import NotAllow from './src/components/views/Admin/AddPost/notallow';
+import SidedrawerComponenent from './src/components/views/Sidedrawer';
+import UserPosts from './src/components/views/Admin/UserPosts';
+import Article from './src/components/views/Article';
 
-type Props = {};
+const store = ConfigureStore();
 
-class App extends Component {
-  state = {
-    name: 'My Awesome App'
-  };
-  render() {
-    return (
-      <View style={styles.container}>
-        <Nav name={this.state.name} />
-        <Text style={styles.mainItemsBckg}>Welcome to React Native!</Text>
-        <Generator />
-      </View>
-    );
-  }
-}
+// Navigation.registerComponentWithRedux( 'sellitApp.Login', () => Login,
+//   store,
+//   Provider
+// );
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-    backgroundColor: '#F5FCFF',
-    paddingTop: 50,
-    width: '100%'
-  },
-  mainItemsBckg: {
-    backgroundColor: 'lightblue',
-    textAlign: 'center',
-    width: ' 100%',
-    marginBottom: 5
-  },
-  basicText: {
-    textAlign: 'center',
-    color: '#ffff',
-    padding: 20,
-    fontSize: 20
-  }
-});
+// Navigation.registerComponentWithRedux('sellitApp.Login', () => (props) => (
+//   <Provider store={store}>
+//     <Login {...props} />
+//   </Provider>
+// ), () => Login);
+Navigation.registerComponentWithRedux(
+  'sellitApp.Login',
+  () => require('./src/components/views/Sidedrawer').default,
+  Provider,
+  store
+);
 
-export default App;
+// Navigation.registerComponentWithRedux('sellitApp.Login', () => (props) => (
+//   Provider,
+//     Login,
+//     store
+// ), () => Login);
+
+// // Navigation.registerComponentWithRedux('blog.PostsList', () => require('./posts/screens/PostsList').default, Provider, store);
+
+// Navigation.registerComponentWithRedux(
+//   'sellitApp.Home',
+//   () => Home,
+//   store,
+//   Provider
+// )
+
+Navigation.registerComponentWithRedux(
+  'sellitApp.Home',
+  () => require('./src/components/views/Home').default,
+  Provider,
+  store
+);
+
+Navigation.registerComponentWithRedux(
+  'sellitApp.AddPost',
+  () => require('./src/components/views/Admin/AddPost').default,
+  Provider,
+  store
+);
+
+Navigation.registerComponentWithRedux(
+  'sellitApp.SidedrawerComponenent',
+  () => require('./src/components/views/Sidedrawer').default,
+  Provider,
+  store
+);
+
+Navigation.registerComponentWithRedux(
+  'sellitApp.UserPosts',
+  () => require('./src/components/views/Admin/UserPosts').default,
+  Provider,
+  store
+);
+
+Navigation.registerComponentWithRedux(
+  'sellitApp.Article',
+  () => require('./src/components/views/Article').default,
+  Provider,
+  store
+);
+
+Navigation.registerComponentWithRedux(
+  'sellitApp.NotAllow',
+  () => require('./src/components/views/Admin/AddPost/notallow').default,
+  Provider,
+  store
+);
+
+export default () =>
+  Navigation.events().registerAppLaunchedListener(() => {
+    Navigation.setRoot({
+      root: {
+        stack: {
+          children: [
+            {
+              component: {
+                name: 'sellitApp.Login',
+                passProps: {
+                  text: 'stack with one child'
+                }
+              }
+            }
+          ],
+          options: {
+            topBar: {
+              title: {
+                text: 'Welcome To Login'
+              }
+            }
+          }
+        }
+      }
+    });
+  });
